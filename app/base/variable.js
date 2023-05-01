90b876860408b665c0b84ab061aaab978e3bf7a7
nayarawatanuki <nayarawatanuki@gmail.com>
2023-05-01 01:44:08 -0300

    project done

--- app/base/variable.js
+++ app/base/variable.js
@@ -1,3 +1,10 @@
 const zipCode = document.getElementById("zipCode");
+const messageErrorZipCode = document.getElementById("zipCode-error");
 
-export { zipCode }
+const street = document.getElementById("street");
+const neighborhood = document.getElementById("neighborhood");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+
+
+export { zipCode, messageErrorZipCode, street, neighborhood, city, state }
